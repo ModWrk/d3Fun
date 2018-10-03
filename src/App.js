d15031f54b2281878bda212282ef5c58bf7b1ac4
ModWrk <erik@modwrk.com>
2018-10-03 12:57:17 -0600

    checkin ahead of dependecy injection

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-
-class App extends React.Component {
-    handleTitleClick() {
-        alert("what up!!");
-    }
-    render() {
-        return React.creatElement("div", {},
-        React.creatElement('h1', {onClick: this.handleTitleClick}, 'App Lives!')
-    )}
+const NewComponent = () => {
+    return React.creatElement("div", {}, [
+        React.creatElement("h2", {}, "Component 1 Heading"),
+        React.creatElement("h2", {}, "Component 2 Heading"),
+        React.creatElement("h2", {}, "Component 3 Heading")
+    ]);
 }
+
+const App = () => {
+    return React.creatElement("div", {}, [
+        React.creatElement('h1', {}, 'Page Title'),
+        React.creatElement(NewComponent),
+        React.creatElement(NewComponent),
+        React.creatElement(NewComponent)
+    ]);
+}
+
+
+// class App extends React.Component {
+//     handleTitleClick() {
+//         alert("what up!!");
+//         console.log("console");
+//     }
+//     render() {
+//         return React.creatElement("div", {},
+//         React.creatElement('h1', {onClick: this.handleTitleClick}, 'Click Works !')
+//     )}
+// }
 
 ReactDOM.render(React.creatElement(App), document.getElementById('root')) 

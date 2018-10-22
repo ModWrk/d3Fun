810216eb893857b76626db3e8a1e08e067df2b68
ModWrk <erik@modwrk.com>
2018-10-22 15:11:12 -0600

    add gyronorm

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Adopt Me!</h1>
+                <h1></h1>
 
             </div>
         )
